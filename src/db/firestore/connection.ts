0a9c667ecf77584edf6ac6e2a0a3da7e1c88bfed
yurisilva <yurikgs@outlook.com>
2023-05-18 06:23:27 -0300

    FIX: env

--- src/db/firestore/connection.ts
+++ src/db/firestore/connection.ts
@@ -1,6 +1,4 @@
 import { getFirestore } from 'firebase/firestore'
 import { firebaseApp } from './firebase'
 
-console.log({env: process.env})
-
 export const connection = getFirestore(firebaseApp)
--- src/db/firestore/firebase.js
+++ src/db/firestore/firebase.js
@@ -9,14 +9,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+
+
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId:  process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCqL1gElZv5v73KxuDdmVrEFwbJaHa2has",
+  authDomain: "territorio-saude.firebaseapp.com",
+  projectId:  "territorio-saude",
+  storageBucket: "territorio-saude.appspot.com",
+  messagingSenderId: "175767385113",
+  appId: "1:175767385113:web:7b8f8d2a01648299c6e688",
+  measurementId: "G-3Y2DBEPKC6",
 };
 
 // Initialize Firebase
